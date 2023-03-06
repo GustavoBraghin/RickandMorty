1c1444ac962443ae590cba12008e24e4759e92f6
Gustavo Braghin <gugabraghin@gmail.com>
2023-03-06 10:23:59 -0300

    Initial Commit

---
+++ RickandMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickandMorty
+//
+//  Created by Gustavo da Silva Braghin on 06/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
